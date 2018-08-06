718789776c7427fe2d92720320572ef169d7afbf
spimp <ks760@cam.ac.uk>
2018-08-06 14:04:45 +0900

    get_features tested and fixed

--- camera_pose.py
+++ camera_pose.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
 #just using this variable for testing purposes
@@ -12,27 +13,36 @@ def get_feature_match(camFrame, projFrame):
     #Get the images and convert them to gray scale
     #img1 = cv2.imread(camFrame)
     img1 = camFrame
-    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    camera_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
+    #switch these round later, this was just for testing purposes
     img2 = cv2.imread(projFrame)
     #img2 = projFrame
-    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    proj_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
     #Consider trying to actually get sift to work here
     #initialise orb which is similar to sift but free
     orb = cv2.ORB_create()
     #find the keypoints and descriptors using orb
-    kp1, des1 = orb.detectAndCompute(gray1, None)
-    kp2, des2 = orb.detectAndCompute(gray2, None)
+    camera_kp, des1 = orb.detectAndCompute(camera_gray, None)
+    proj_kp, des2 = orb.detectAndCompute(proj_gray, None)
 
     #Brute-Force matcher
     #BFMatcher with default params
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
 
+    #Test for the matcher
+    """good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+    # cv2.drawMatchesKnn expects list of lists as matches.
 
+    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None,  flags=2)
+    plt.imshow(img3), plt.show()"""
 
-    return matches, kp1, kp2
+    return matches, camera_kp, proj_kp
 
 #####FLANN based matcher didn't work :( Maybe because I'm using orb instead of sift
     #FLANN based matcher
@@ -55,7 +65,8 @@ def get_feature_match(camFrame, projFrame):
     #return good, kp1, kp2
 
 #Function to find the homography matrix which transforms from the camera image to the projector image
-def get_homography(matches, kp1, kp2):
+def get_homography(matches, camera_kp, proj_kp):
+    homography_matrix = np.array[]
     #Apply ratio test to get good matches
     good = []
     for m,n in matches:
@@ -65,16 +76,20 @@ def get_homography(matches, kp1, kp2):
     #check that there is enough matches
     if len(matches)>MIN_MATCH_COUNT:
         #Not really sure what this is doing, what are queryIdx and trainIdx
-        src_pts = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+        src_pts = np.float32([proj_kp[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+        dst_pts = np.float32([camera_kp[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
         #get the homography matrix
-        matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        homography_matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-        return matrix
+        return homography_matrix
     else:
+        print()
         #if a homography matrix could not be found return 0. I am finding it hard to test for this.
         #Need something we can test for later in cases where there weren;t enough matches
+        #INitialise the matrix with zeros at the start of this function and just return the empty matrix if it goes into
+        #the else case
+        #Should also set mask = None or something so there are no errors  in the future
         return np.array(np.array[0, 0])
 
 #returns the location of the centre of the camera image in the projector image
@@ -96,10 +111,20 @@ def virtual_point(camFrame, hgmatrix):
 
 ###################Main piece of code
 
+
 #initialise the webcam
 cap = cv2.VideoCapture(1)
+ret = False
+while ret == False:
+    ret, frame = cap.read()
+    matches, kp1, kp2 = get_feature_match(frame, projFrame)
 
-while (True): #if this is an infinite loop do all other functions have to be called from here.
+    hgmatrix = get_homography(matches, kp1, kp2)
+    print(hgmatrix)
+
+
+
+"""while (True): #if this is an infinite loop do all other functions have to be called from here.
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -111,9 +136,11 @@ while (True): #if this is an infinite loop do all other functions have to be cal
     display = cv2.imread(projFrame)
 
     #if hgmatrix:
-    print(hgmatrix.shape)
+    #print(hgmatrix.shape)
     #get the location of the centre of the camera image in the projector image
     dspPoint = virtual_point(frame, hgmatrix)
+    print(dspPoint)
+    dspPoint = tuple(dspPoint.reshape(1, -1)[0])
     print(dspPoint)
     # draw a circle on where we clicked
     cv2.circle(display, dspPoint[0], 3, (0, 0, 255), thickness=-1, lineType=8)  # color BGR
@@ -125,7 +152,7 @@ while (True): #if this is an infinite loop do all other functions have to be cal
     cv2.imshow('projector', display)
     if cv2.waitKey(1) & 0xFF == 0x1b:  # ord('q'):
         break
-
+"""
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
