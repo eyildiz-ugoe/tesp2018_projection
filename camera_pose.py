96ed9e0fa633e8be9a40d4dfae42e3209fe9e604
Erenus Yildiz <erenus.yildiz@iais.fraunhofer.de>
2018-08-07 13:52:30 +0200

    fixed the bug, now homography works

--- camera_pose.py
+++ camera_pose.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-# settings
+# Settings
 background_height = 350
 background_width = 612
 MIN_MATCH_COUNT = 6
@@ -9,12 +9,12 @@ MAX_MATCH_COUNT = 20
 CAM_WIDTH = 1280
 CAM_HEIGHT = 960
 
-#just using this variable for testing purposes
+# Just using this variable for testing purposes
 imageToBeProjected = 'solar_system.jpg'
 
 # marker stuff
 marker_file_name = ["marker_one_small.png","marker_two_small.png","marker_three_small.png","marker_four_small.png"]
-marker_points = [[0,0],[0,background_height-100],[background_width-100,0],[background_width-100,background_height-100]]
+marker_points = [[0, 0], [0, background_height-100], [background_width-100, 0], [background_width-100, background_height-100]]
 
 def init_webcam(mirror=False):
     cam = []
@@ -40,26 +40,26 @@ def init_webcam(mirror=False):
     return cam, camera_height, camera_width
 
 
-#Function to find the homography matrix which transforms from the camera image to the projector image
+# Function to find the homography matrix which transforms from the camera image to the projector image
 def get_homography(matches, projectionImage_kp, cameraImage_kp):
+
     homographyMatrix = []
 
     # Only perform this if there are enough matches
     if len(matches) > MIN_MATCH_COUNT:
-        try:
-            src_pts = np.float32([projectionImage_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-            dst_pts = np.float32([cameraImage_kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+        # Taken from the tutorial
+        src_pts = np.float32([projectionImage_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([cameraImage_kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-            # get the homography matrix
-            homographyMatrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            matchesMask = mask.ravel().tolist()
-        except IndexError:
-            print("ERROR: Adjust camera angle")
+        # Get the homography matrix with RANSAC
+        homographyMatrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
     else:
         print("Not enough matches are found - %d/%d" % (len(matches), MIN_MATCH_COUNT))
         matchesMask = None
 
     return homographyMatrix, matchesMask
+
 
 def show_matches(projectionImage, cameraImage, projectionImage_kp, cameraImage_kp, homographyMatrix):
     h,w,d = projectionImage.shape
@@ -75,6 +75,7 @@ def show_matches(projectionImage, cameraImage, projectionImage_kp, cameraImage_k
     visualizationImage = cv2.drawMatches(projectionImage, projectionImage_kp, cameraImage, cameraImage_kp, matches, None, **draw_params)
     cv2.imshow('Debug', visualizationImage)
 
+
 # Returns the location of the centre of the camera image in the projector image
 def virtual_point(homographyMatrix):
     pts = np.float32([ [round(CAM_WIDTH/2),round(CAM_HEIGHT/2)] ]).reshape(-1,1,2)
@@ -82,6 +83,7 @@ def virtual_point(homographyMatrix):
     # find the location of this same point in the projector image
     dst = cv2.perspectiveTransform(pts, m[1])
     return dst
+
 
 if __name__ == '__main__':
 
@@ -105,7 +107,7 @@ if __name__ == '__main__':
     orb = cv2.ORB_create(nfeatures=500)
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    # Create an opencv window to display the projection into
+    # Create an opencv window to display the projection onto
     cv2.namedWindow("Projector", cv2.WINDOW_NORMAL)
     cv2.imshow('Projector', projectionImage)
     cv2.namedWindow("Debug", cv2.WINDOW_NORMAL)
@@ -113,8 +115,7 @@ if __name__ == '__main__':
     # work with a copy
     processedImage = projectionImage.copy()
 
-    while(True):
-
+    while True:
         orb2 = cv2.ORB_create(nfeatures=500)
         projectionImage_kp = orb2.detect(processedImage, None)
         projectionImage_kp, projectionImage_des = orb2.compute(processedImage, projectionImage_kp)
@@ -125,6 +126,7 @@ if __name__ == '__main__':
         # Find the keypoints and descriptors with ORB
         cameraImage_kp = orb.detect(cameraImage, None)
 
+        # Compute matches
         matches = []
         if len(cameraImage_kp) > 0:
             cameraImage_kp, cameraImage_des = orb.compute(cameraImage, cameraImage_kp)
@@ -133,7 +135,7 @@ if __name__ == '__main__':
                 matches = sorted(matches, key=lambda x: x.distance)[0:MAX_MATCH_COUNT]
 
         # if we can't find any matches, just keep displaying the image and inform the user
-        if(len(matches) <= MAX_MATCH_COUNT):
+        if len(matches) <= MIN_MATCH_COUNT:
             cv2.imshow('Projector', processedImage)
             print('Could not find matches.')
             cv2.waitKey(10)
@@ -141,22 +143,18 @@ if __name__ == '__main__':
 
         # Now compute the Homography
         homographyMatrix, matchesMask = get_homography(matches, projectionImage_kp, cameraImage_kp)
-        if len(homographyMatrix)==0:
-            print("No homography matrix calculated")
-            continue
 
         # Visualize!
         show_matches(processedImage, cameraImage, projectionImage_kp, cameraImage_kp, homographyMatrix)
 
         # Draw a virtual point on the image
         dspPoint = virtual_point(homographyMatrix)
-
-        # draw a circle on where we clicked
-        cv2.circle(projectionImage, dspPoint, 20, (0, 0, 255), thickness=1, lineType=8)  # color BGR
+        new_point = dspPoint[0][0]
+        cv2.circle(projectionImage, tuple(new_point), 20, (0, 0, 255), thickness=1, lineType=8)  # color BGR
 
         # Display the resulting projector image with a dot for the camera location
         cv2.imshow('Projector', processedImage)
-        if cv2.waitKey(33) == ord('a'):
+        if cv2.waitKey(20) == ord('a'):
             break
 
     # When everything done, release the capture
