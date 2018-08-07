c7d996f4e105a22150c76ceb3ebed0f88646ec83
Erenus Yildiz <erenus.yildiz@iais.fraunhofer.de>
2018-08-07 15:10:26 +0200

    Katie's and Eren's parts are now integrated.

--- camera_pose.py
+++ camera_pose.py
@@ -1,5 +1,14 @@
-import numpy as np
 import cv2
+import pygame
+import numpy as np
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+import xml.etree.ElementTree as ET
+from pygame import mixer # Load the required library
+
+# for the sound stuff
+pygame.mixer.init()
 
 # Settings
 projectedImageHeight = 350
@@ -14,13 +23,111 @@ trackedCenterPoint = [0, 0]
 trackingVelocity = [0, 0]
 smoothenedMatrix = np.float32([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
+# Global variable to hold all celestial bodies
+planets = stars = planet_list = []
 
-# Just using this variable for testing purposes
+# Just using this dummy image for testing purposes
 imageToBeProjected = 'solar_system.jpg'
 
 # marker stuff
-marker_file_name = ["marker_one_small.png","marker_two_small.png","marker_three_small.png","marker_four_small.png"]
+marker_file_name = ["markers/marker_one_small.png","markers/marker_two_small.png","markers/marker_three_small.png","markers/marker_four_small.png"]
 marker_points = [[0, 0], [0, projectedImageHeight - 100], [projectedImageWidth - 100, 0], [projectedImageWidth - 100, projectedImageHeight - 100]]
+
+
+"""
+Planet class to make things easier to handle.
+
+"""
+class Planet(object):
+    name = ""
+    distanceFromEarth = 0 #in lightyears
+    size = 0 # multiplier only. x times of earth's
+    gravity = 0 # multiplier only. x times of earth's
+    moons = [] # only the names
+    compoundFound = []
+    orbitTime = 0 # in days (earth)
+    dayTime = 0 # in days (earth)
+
+    def __init__(self, name, distanceFromEarth, size, moons, gravity, compoundFound, orbitTime, dayTime):
+        self.name = name
+        self.distanceFromSun = distanceFromEarth
+        self.size = size
+        self.gravity = gravity
+        self.moons = moons
+        self.compoundFound = compoundFound
+        self.orbitTime = orbitTime
+        self.dayTime = dayTime
+
+# Text to display about the celestial body
+def prepare_info(planet):
+
+    info = "-Celestial Body Info" \
+           "\n--Name: " + str(planet.name) + \
+           "\n--Distance from the Earth: " + str(planet.distanceFromEarth) + " lightyears" + \
+           "\n--Size: " + str(planet.size) + " x of Earth" + \
+           "\n--Gravity: " + str(planet.gravity) + " x of Earth" + \
+           "\n--Moons: " + str(planet.moons) + \
+           "\n--Compounds Found: " + str(planet.compoundFound) + \
+           "\n--Orbit Time: " + str(planet.orbitTime) + " Earth days" + \
+           "\n--Day Time: " + str(planet.dayTime) + " Earth days"
+
+    return info
+
+# Keep this out for now. This event handler will be used when the animation part is integrated.
+# The main purpose of this is to find out when the camera point is over a certain celestial body
+# It displays the information once it is the case.
+"""
+def hover_and_display(event, x, y, flags, param):
+    # grab references to the global variables
+    global processedImage
+
+    #TODO: We need to get a new image frame every time the user clicks,
+    #TODO: otherwise we keep writing on the same image
+
+    # if the camera point is over the celestial body points
+    if event == cv2.EVENT_LBUTTONUP:
+
+        # display info
+        #TODO: Here we need to find out which planet is selected,
+        #TODO: by getting the coordinates from the image.
+        #TODO: However, we need to find a clever way, unlike what is shown below
+        if(x == 100 && y == 150):
+            planet = planet_list.find('Mercury')
+        elif(x == 200 && y == 250):
+            planet = planet_list.find('Venus')
+
+        # play the info effect
+        #click_effect = pygame.mixer.Sound('sounds/info.wav')
+        #click_effect.play()
+
+
+        # for the time being we only print the info of the sun, which is the 0th element
+        # once we figure out which celestial body is selected, we need to pass that specific planet with:
+        # info = prepare_info(planet)
+        info = prepare_info(planet_list[0]) # index 0 is the sun
+
+        # get the font
+        fontsize = 10
+        font = ImageFont.truetype("spacefont.ttf", fontsize)
+
+        # load the image to PIL format
+        img_pil = Image.fromarray(img)
+
+        # draw the font
+        draw = ImageDraw.Draw(img_pil)
+        clickingOffset = 50 # how far should the information be displayed (in pixels)
+        draw.text((x + clickingOffset, y), info, font=font, fill=(0,0,255,0)) # color BGR
+
+        # back to opencv format
+        processedImage = np.array(img_pil)
+
+        # add a line to the info
+        textOffset = 90 # enough long to cover the text body on X axis
+        cv2.line(processedImage, (x, y), (x+clickingOffset + textOffset, y), (0, 0, 255), 1)
+
+        # display it
+        cv2.imshow("Projector", processedImage)
+"""
 
 def init_webcam(mirror=False):
     cam = []
@@ -141,6 +248,34 @@ def smoothenCenterMotion(measured_position, delta_t):
 
 if __name__ == '__main__':
 
+    # play the background sound
+    mixer.music.load('sounds/background.mp3')
+    mixer.music.play(-1)
+
+    # Get the data from the XML
+    solarSystem = ET.parse('planet_info.xml')
+    celestialBodies = solarSystem.getroot()
+
+    # Parse everything from XML into global variables
+    for cBodies in celestialBodies:
+        planets = cBodies.findall("planet")
+        stars = cBodies.findall("star")
+        if planets:
+            for planet in planets:
+                planet_list.append(
+                    Planet(planet[0].text, planet[1].text, planet[2].text, planet[3].text, planet[4].text,
+                           planet[5].text, planet[6].text, planet[7].text))
+        elif stars:  # since there is only one star (sun), we just add it into the list of planets
+            for star in stars:
+                planet_list.append(
+                    Planet(star[0].text, star[1].text, star[2].text, star[3].text, star[4].text, star[5].text,
+                           star[6].text, star[7].text))
+        else:
+            print("Nothing")
+
+    #TODO: Leave it like this for now. Comment it in once the animation part is integrated.
+    #cv2.setMouseCallback("Projector", hover_and_display)
+
     # Set up the camera
     cam, camera_height, camera_width = init_webcam()
     CAM_WIDTH = camera_width
@@ -203,10 +338,12 @@ if __name__ == '__main__':
             updatedPoint = virtualPoint[0][0]
         else:
             updatedPoint = [p for p in trackedCenterPoint]
-            print("Failed to prevent scattering")
 
-        smoothenCenterMotion(updatedPoint, DELTA_T) # we don't want scattering or abrupt weird moves, so smoothen the motion
-        cv2.circle(processedImage, tuple(trackedCenterPoint), 10, (0, 0, 255), thickness=1, lineType=8)  # draw it
+        # we don't want scattering or abrupt weird moves, so smoothen the motion
+        smoothenCenterMotion(updatedPoint, DELTA_T)
+
+        # draw a circle on the image
+        cv2.circle(processedImage, tuple(trackedCenterPoint), 5, (0, 0, 255), thickness=1, lineType=8)  # draw it
 
         # Display the resulting projector image with a dot for the camera location
         cv2.imshow('Projector', processedImage)
